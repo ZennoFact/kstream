a44574564528a9f44d2be16f523d4a7d4bb80bd0
Kazuki Maeno <zenno04.g@gmail.com>
2015-07-22 15:36:50 +0900

    Add Skyway Session
    Use Skyway by NTT Communications.
    Add 2 files and 1 library.Brodcaster page, audience page and JQuery.

---
+++ audience.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>K STREAM!!</title>
+    <script src="./js/library/jquery-2.1.3.min.js"></script>
+    <script src="https://skyway.io/dist/0.3/peer.js"></script>
+  </head>
+  <body>
+    <h1>現在，前納の動画を配信中</h1>
+    <video id="video_box" autoplay="autoplay" width="500px" height="250px">
+
+    <script>
+      navigator.getUserMedia = navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia;
+      var peer = new Peer({key: '405bf42c-a370-423f-a923-07820ec6032d'});
+      peer.on("error", function(e) {
+        console.log(e);
+      });
+
+      navigator.getUserMedia({audio: true, video: true}, function (stream) {
+        console.log(peer);
+        console.log(stream);
+        var call = peer.call("256", stream);
+        console.log(call);
+
+        call.on("stream", function (talk_stream) {
+          $("#video_box").attr("src", URL.createObjectURL(talk_stream));
+        });
+      }, function(error){});
+    </script>
+  </body>
+</html>
